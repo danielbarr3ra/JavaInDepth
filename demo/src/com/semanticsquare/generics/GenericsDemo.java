92d69a0cefe732df9ba376cc5a539697590563b4
danielbarr3ra <49797284+danielbarr3ra@users.noreply.github.com>
2021-03-29 23:59:51 -0500

    More examples for bounded generics and rawtypes

--- demo/src/com/semanticsquare/generics/GenericsDemo.java
+++ demo/src/com/semanticsquare/generics/GenericsDemo.java
@@ -1,9 +1,11 @@
 package com.semanticsquare.generics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class GenericsDemo {
+
+public class GenericsDemo <T extends ArrayList > {
 	public static void main(String[] args) {
 		
 		Container<String> stringStore = new Store<>();
@@ -18,6 +20,39 @@ public class GenericsDemo {
 		Container<List<Integer>> listStore = new Store<>();
 		listStore.set(Arrays.asList(1,2,3));
 		System.out.println(listStore.get());
+		
+		GenericsDemo<ArrayList> list1 = new GenericsDemo<>();
+		
+		//
+		//rawTypeTest();
+		List<String> strList1 = Arrays.asList("a","b","c");
+		List<String> strList2 = Arrays.asList("b","c","d");
+		getCommonElementsCount(strList1,strList2);
+
+	}
+	
+	public static int getCommonElementsCount(List<String> list1, List<String> list2) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		for (Object element : list1) {
+			if (list2.contains(element)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void rawTypeTest() {
+		// TODO Auto-generated method stub
+		System.out.println("raw type test");
+		int ISBN = 1303030;
+		List<Double> prices = new ArrayList<>();
+		HalfIntegrator.getPrice(ISBN,prices);
+		Double price = prices.get(0);
+	}
+
+	public void go(T list) {
+		list.add(0, new Object());
 	}
 }
 
@@ -26,6 +61,11 @@ interface Container<T>{
 	T get();
 }
 
+class HalfIntegrator {
+	public static void getPrice(int ISBN, List<Double> prices) {
+		prices.add((double) 45);
+	}
+}
 class Store<T> implements Container<T>{
 	private T a;
 	public void set(T a) {
